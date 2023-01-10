dee67a3ba8e2a81a9ad7f1f6cab4b4bbb704b9be
MakashevaZa <zarina.ma.93@yandex.ru>
2023-01-10 16:53:24 +0300

    TB-0: Added SpringBoot skeleton of a project

---
+++ src/main/java/com/github/MakashevaZa/tb/MakashevaZaApplication.java
@@ -0,0 +1,13 @@
+package com.github.MakashevaZa.tb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakashevaZaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakashevaZaApplication.class, args);
+	}
+
+}
